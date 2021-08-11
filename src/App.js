e05809137590c851b091a7887fa637f7d7fa2a5e
KangHeeWon <kkhw1202@gmail.com>
2021-08-11 17:41:20 +0900

    Add delay function

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
 import React from "react";
-// import PropTypes from "prop-types";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('hello');
-  }
   state = {
-    count: 0,
-  };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
+    isLoading: true,
+    movies: [],
   };
 
   componentDidMount() {
-    console.log('component rendered');
+    // delay function
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
   }
-  componentDidUpdate() {
-    console.log('i just updated');
-  }
-  componentWillUnmount() {
-    console.log('goodbye');
-  }
+
   render() {
-    console.log('i am render');
-    return (
-      <div>
-        <h1>the number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'loading...' : 'we are ready'}</div>;
   }
 }
 
