78c6840d926f63a1c53e51457040b239d967804b
KangHeeWon <kkhw1202@gmail.com>
2021-08-26 16:14:42 +0900

    Add contents in About component, import css

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-    return <span>About this page: i built it because i love movies.</span>
+    return (
+        <div className="about_container">
+            <p>
+                <em>About this page</em>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis consequatur corporis debitis, nisi eos, perspiciatis sint asperiores quisquam animi at voluptates consectetur? Quae incidunt pariatur assumenda inventore in doloremque praesentium!
+            </p>
+        </div>
+    )
 }
 
 export default About;
