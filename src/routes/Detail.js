574c9e3b3b175135f27b9de558024a231fe4de29
KangHeeWon <kkhw1202@gmail.com>
2021-08-28 14:29:25 +0900

    Add Detail contents

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
     componentDidMount() {
@@ -9,8 +10,22 @@ class Detail extends React.Component {
     }
     render() {
         const {location} = this.props;
+        console.log(location.state);
         if(location.state) {
-            return <span>{location.state.title}</span>;
+            return (
+                <div className="detail_wrap">
+                    <h1 className="detail_poster">
+                    <img src={location.state.poster} alt={location.state.title} title={location.state.title} />
+                    </h1>
+                    <h2>{location.state.title} <i>({location.state.year}</i>)</h2>
+                    <ul className="detail_genres">
+                        {location.state.genres.map((genre, index) => <li className="detail_genre" key={index}>{genre}</li>)}
+                    </ul>
+                    <p className="summary">
+                        {location.state.summary}
+                    </p>
+                </div>
+                );
         } else {
             return null;
         }
