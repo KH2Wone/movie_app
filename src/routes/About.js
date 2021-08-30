a6178526d84f0b5f065a61891eb31a846bb0a4ce
KangHeeWon <kkhw1202@gmail.com>
2021-08-30 12:30:13 +0900

    Add About css, content

--- src/routes/About.js
+++ src/routes/About.js
@@ -4,10 +4,12 @@ import "./About.css";
 function About() {
     return (
         <div className="about_container">
-            <p>
-                <em>About this page</em>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis consequatur corporis debitis, nisi eos, perspiciatis sint asperiores quisquam animi at voluptates consectetur? Quae incidunt pariatur assumenda inventore in doloremque praesentium!
-            </p>
+            <h1>About Movie-App (2021)</h1>
+            <ul className="about_summary">
+                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio inventore quaerat illo, libero nostrum suscipit consequatur reprehenderit, repudiandae, nobis exercitationem facilis repellendus porro corporis ratione veritatis non omnis neque dolores?</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ratione eos facilis nesciunt cumque, ut soluta deserunt velit rem atque, quasi cupiditate iure est alias. Dicta aliquid vero neque rerum dolore. Fugiat quod cupiditate autem? Deserunt laborum reiciendis, omnis cum nesciunt dolorem sit facere harum reprehenderit ab maiores libero, minima quisquam ipsum adipisci mollitia animi minus aspernatur saepe, expedita provident aut. Totam, exercitationem ab sapiente eum, molestiae et natus repellendus maxime, laborum quisquam similique doloremque voluptate corporis! Enim modi necessitatibus vel numquam aut. Maiores quos eligendi illum ut enim eos exercitationem a tenetur totam ullam? Adipisci aut fugiat sequi beatae.</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptas voluptates odit quod explicabo, sapiente aut vel voluptatibus possimus sed iste eveniet minus, odio at, quae est quos ex. Accusamus ducimus consequatur obcaecati molestias nihil sint nobis eaque deserunt, sequi omnis repellendus dignissimos, explicabo quia eius velit libero repellat modi.</li>
+            </ul>
         </div>
     )
 }
